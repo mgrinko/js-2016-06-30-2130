76416abe6347337e886ade1f5b77860b93c6ca18
mgrinko <glukym@gmail.com>
2016-08-15 22:39:38 +0300

    Phones are now rendered

--- js/phoneCatalogue.js
+++ js/phoneCatalogue.js
@@ -4,6 +4,26 @@ class PhoneCatalogue {
   constructor(options) {
     this._el = options.element;
 
+    this._render(options.phones);
+  }
 
+  _render(phones) {
+    let html = '<ul class="phones">';
+
+    phones.forEach(function(phone) {
+      html += `
+        <li class="thumbnail">
+          <a href="#!/phones/${phone.id}" class="thumb">
+            <img alt="${phone.name}" src="${phone.imageUrl}">
+          </a>
+          <a href="#!/phones/${phone.id}">${phone.name}</a>
+          <p>${phone.snippet}</p>
+        </li>
+      `;
+    }.bind(this));
+
+    html += '</ul>';
+
+    this._el.innerHTML = html;
   }
 }
