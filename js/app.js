ffa205898c1fcdb6c9817d195140a52458df02b5
mgrinko <glukym@gmail.com>
2016-08-15 22:13:52 +0300

    Phone catalogue basic

---
+++ js/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let page = new PhonePageController({
+  element: document.getElementById('phone-catalogue-page')
+});
+
+
+
+
---
+++ js/phoneCatalogueController.js
@@ -0,0 +1,23 @@
+'use strict';
+
+class PhonePageController {
+  constructor(options) {
+    this._el = options.element;
+
+    this._filter = new Filter({
+      element: this._el.querySelector('[data-component="filter"]')
+    });
+
+    this._sorter = new Sorter({
+      element: this._el.querySelector('[data-component="filter"]')
+    });
+
+    this._phoneCatalogue = new PhoneCatalogue({
+      element: this._el.querySelector('[data-component="filter"]')
+    });
+
+    this._phoneViewer = new PhoneViewer({
+      element: this._el.querySelector('[data-component="filter"]')
+    });
+  }
+}
